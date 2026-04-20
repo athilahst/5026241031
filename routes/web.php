d61d31916b1c0314c44009822d07b7f3c3cbe734
Athilah <athilah.st@gmail.com>
2026-04-20 09:49:34 +0700

    pertemuan7

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+    return "<h1>Halo, Selamat datang</h1> di tutorial Laravel <i>www.malasngoding.com</i>";
+});
+
+Route::get('blog', function () {
+    return view('blog');
+});
+
+Route::get('pert5', function () {
+    return view('pertemuan5');
+});
+
+Route::get('dosen', [DosenController::class, 'index']);
+
+Route::get('bio', function () {
+    return view('biodata');
+});
